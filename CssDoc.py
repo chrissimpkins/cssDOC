e63ce357615d5b981a3feef9385a5dbe844ce26d
Chris <git.simpkins@gmail.com>
2014-03-19 12:53:15 -0400

    updated with 'hover' search for CSS elements

--- CssDoc.py
+++ CssDoc.py
@@ -13,38 +13,159 @@ class CssDocCommand(sublime_plugin.TextCommand):
     def run(self, selected_text):
         try:
             selections = self.view.sel()
-            if selections:
+            if selections and (len(selections[0]) > 1): # text was selected in the editor
                 needle = self.view.substr(selections[0])
-                if len(needle) == 0:
-                    print("cssDOC Plugin Error: There was no selected string for the CSS documentation search.  Please select a string in your document and try again.")
+                needle = clean_query(needle) # clean the query string
+                if is_a_css_entity(needle):
+                    self.launch_doc_browser(needle) # launch the documentation page
                 else:
-                    needle = clean_query(needle) # clean the query string
-                    if version_info[0] == 2:     # Python 2 (ST 2)
-                        if is_a_css_entity(needle):
-                            needle_escaped = urllib.quote(needle)
-                            url = "http://developer.mozilla.org/en-US/docs/Web/CSS/" + needle_escaped
+                    self.launch_search_browser(needle) # launch a search page
+            else: # 'hover' handling (cursor located in the CSS element substring)
+                parsed_string = self.find_query_hover()
+                cleaned_string = clean_query(parsed_string)
+                if is_a_css_entity(cleaned_string):
+                    self.launch_doc_browser(cleaned_string)
+                else:
+                    if cleaned_string.startswith(':'): # attempt to add second colon and test for CSS pseudoelement
+                        pseudoelement = ':' + cleaned_string
+                        if is_a_css_entity(pseudoelement):
+                            self.launch_doc_browser(pseudoelement)
                         else:
-                            needle_escaped = urllib.quote(needle)
-                            url = "http://developer.mozilla.org/en-US/search?q=" + needle_escaped + "&topic=css" # url encode Python 2 interpreter
-                    else:                        # Python 3 (ST 3)
-                        if is_a_css_entity(needle):
-                            url = "http://developer.mozilla.org/en-US/docs/Web/CSS/" + needle
-                        else:
-                            url = "http://developer.mozilla.org/en-US/search?q=" + needle + "&topic=css"
-                        url = urllib.parse.urlparse(url).geturl() # url encode Python 3 interpreter
-                    print("cssDOC: Performing search for CSS documentation on the query, '" + needle + "'")
-                    webbrowser.open(url)
-            else:
-                print("cssDOC Plugin Error: There was no selected string for the search.  Please select a string in your document and try again.")
+                            print('cssDOC Plugin Error: Unable to identify the requested text as a CSS element')
+                            status_message = "cssDOC: Unable to match the selected substring"
+                            sublime.status_message(status_message) # print error message to status bar
+                    else:
+                        print('cssDOC Plugin Error: Unable to identify the requested text as a CSS element')
+                        status_message = "cssDOC: Unable to match the selected substring"
+                        sublime.status_message(status_message) # print error message to status bar
         except Exception as e:
             print("cssDOC Plugin Error: There was an error during the execution of the plugin.\n")
             raise e
 
+    # launch documentation URL in browser
+    def launch_doc_browser(self, needle):
+        if version_info[0] == 2:   # Python 2 (ST 2)
+            # needle_escaped = urllib.quote(needle)
+            url = "http://developer.mozilla.org/en-US/docs/Web/CSS/" + needle
+        else: # Python 3 (ST 3)
+            url = "http://developer.mozilla.org/en-US/docs/Web/CSS/" + needle
+            # url = urllib.parse.urlparse(url).geturl() # url encode Python 3 interpreter
+        print("cssDOC: Opening CSS documentation for '" + needle + "'")
+        webbrowser.open(url)
+
+    # launch search URL in browser
+    def launch_search_browser(self, needle):
+        if version_info[0] == 2: # Python 2 (ST 2)
+            # needle_escaped = urllib.quoste(needle)
+            url = "http://developer.mozilla.org/en-US/search?q=" + needle + "&topic=css" # url encode Python 2 interpreter
+        else: # Python 3 (ST 3)
+            url = "http://developer.mozilla.org/en-US/docs/Web/CSS/" + needle
+            # url = urllib.parse.urlparse(url).geturl() # url encode Python 3 interpreter
+        print("cssDOC: Performing search for CSS documentation on '" + needle + "'")
+        webbrowser.open(url)
+
+    # find the query term if 'hover' over the element
+    def find_query_hover(self):
+        the_selection = self.view.sel()
+        start = the_selection[0].begin()
+        begin_test = start
+        end_test = start
+        doc_end_point = self.view.size()
+
+        # find end of the CSS entity
+        for x in range(50):
+            if end_test < doc_end_point:
+                end_test += 1
+                end_region = sublime.Region(start, end_test)
+                end_test_string = self.view.substr(end_region)
+                if end_test_string.endswith(':'): # the end of the CSS element before assignments
+                    end_test -= 1 # remove the colon at the end of the assignment
+                    break
+                elif end_test_string.endswith(';'): # the end of a CSS statement
+                    end_test -= 1 # remove the semicolon at the end of the CSS statement
+                    break
+                elif end_test_string.endswith('{'): # before beginning of a CSS block
+                    end_test -= 1 # remove the opening { from the CSS block
+                    break
+                elif end_test_string.endswith(' '): # ends with a space (likely an element prior to the {} block declaration)
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith('\n'): # end of line
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith(','): # comma between elements
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith('('): # beginning of a parenthesis block
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith('+'): # + operator
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith('>'): # > operator
+                    end_test -= 1
+                    break
+                elif end_test_string.endswith('~'): # ~ operator
+                    end_test -= 1
+                    break
+            else:
+                end_test = self.view.size() # end of buffer text
+
+        # find beginning of the CSS entity
+        for x in range(50):
+            if begin_test > 0:
+                begin_test -= 1
+                begin_region = sublime.Region(begin_test, start)
+                begin_test_string = self.view.substr(begin_region)
+                if begin_test_string.startswith(':'):
+                    begin_test2 = begin_test - 1 # test for a second colon at beginning of the element
+                    if begin_test2 > 0:
+                        begin_region_doublecolon = sublime.Region(begin_test2, start)
+                        begin_test_string_doublecolon = self.view.substr(begin_region_doublecolon)
+                        if begin_test_string_doublecolon.startswith(':'):
+                            begin_test = begin_test2 # assign the start point at the beginning of the first colon and return
+                            break
+                elif begin_test_string.startswith(';'): # the previous CSS statement
+                    break
+                elif begin_test_string.startswith('{'): # the beginning of the CSS block
+                    begin_test += 1 # remove the opening { of the CSS block
+                    break
+                elif begin_test_string.startswith('}'): # the end of another CSS block
+                    begin_test += 1 # remove the closing } of previous block
+                    break
+                elif begin_test_string.startswith('\n'): # the beginning of line/end of prior line
+                    begin_test += 1 # remove the newline
+                    break
+                elif begin_test_string.startswith('('): # likely inline CSS / advanced media queries / etc
+                    begin_test += 1
+                    break
+                elif begin_test_string.startswith('@'):
+                    break # maintain the @ character in the string
+                elif begin_test_string.startswith(' '):
+                    begin_test += 1
+                    break
+            else:
+                begin_test = 0 # beginning of buffer text
+                break
+        # return the parsed substring to the calling code
+        return self.view.substr(sublime.Region(begin_test, end_test))
+
+# clean the CSS query string
 def clean_query(query_string):
     strip_string = query_string.strip()
     if strip_string.endswith(':'):
         strip_string = strip_string[:-1]
         strip_string = strip_string.rstrip()
+    if ';' in strip_string:
+        strip_string = strip_string.replace(';', '')
+        strip_string = strip_string.strip()
+    if ':' in strip_string and not strip_string.startswith(':'):
+        if '::' in strip_string:
+            after_string = strip_string.split('::')[1] # it is a pseudo-element
+            strip_string = '::' + after_string
+        elif ':' in strip_string: # must remain after the above test logic
+            after_string = strip_string.split(':')[1]
+            strip_string = ':' + after_string
     return strip_string
 
 def is_a_css_entity(query_string):
